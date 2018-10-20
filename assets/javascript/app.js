57abad194601377a45512e9fe4e0aa1777c85313
Ahuilaca <santosrodriguez.rcs@gmail.com>
2018-10-20 06:31:12 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,13 @@ $("#start").on("click", function () {
     game.start();
 })
 
-$(document).on("click", '#end', function () {
-    game.done();
+$(document).on("click", '#stop', function () {
+    game.over();
 })
 
-var questions = [{
+
+//Trivia questions, answers, and correct answers==================================
+var triviaQuestions = [{
     question: "In what year was the Aztec Empire founded?",
     answer: [1217, 1825, 1519, 1323],
     correctAnswer: "1323"
@@ -21,67 +23,81 @@ var questions = [{
     correctAnswer: "Huitzilopochtli"
 }];
 
+
+//For final score===================================================================
 var game = {
     correct: 0,
     incorrect: 0,
     counter: 10,
-    countdown: function () {
+
+    //Countdown clock function=======================================================
+    countdownClock: function () {
         game.counter--;
         $("#counter").html(game.counter);
         if (game.counter <= 0) {
             console.log("time is up");
-            game.done();
+            game.over();
         }
     },
-    
+
     start: function () {
-        timer = setInterval(game.countdown, 1000);
-        $("#inner-container").prepend('Time Remaining: <span id="counter">120</span> Seconds');
+        timer = setInterval(game.countdownClock, 1000);
+        $("#inner-container").html("<h2>Time Remaining: <span id='counter'>120</span> Seconds</h2><br><br>");
+
+        //To remove start button======================================================
         $("#start").remove();
-        for (var i=0; i<questions.length; i++) {
-            $("#inner-container").append('<h2>' + questions[i].question + '</h2>');
-            for (var j = 0; j < questions[i].answer.length; j++) {
-                $("#inner-container").append("<input type='radio' name='question-"+i+"' value =' "+questions[i]+"'>" +questions[i].answer[j]);
+
+        //Loop to replace start button with trivia questions==========================
+        for (var i = 0; i < triviaQuestions.length; i++) {
+            $("#inner-container").append('<h2>' + triviaQuestions[i].question + '</h2>');
+
+            for (var j = 0; j < triviaQuestions[i].answer.length; j++) {
+                $("#inner-container").append("<input type='radio' name='question-" + i + "' value =' " + triviaQuestions[i] + "'>" + triviaQuestions[i].answer[j]);
             }
         }
-        $("#inner-container").append('<br><button id="end">Done</button>');
+
+        //Adds the done button at bottom of game======================================
+        $("#inner-container").append('<br><br><br><button id="stop">Done</button>');
     },
-    done: function() {
-        $.each($("input[name='question-0']:checked"), function() {
-            if ($(this).val() == questions[0].correctAnswer) {
+
+    //Adds total when game is over and adds to final score=============================
+    over: function () {
+        $.each($("input[name='question-0']:checked"), function () {
+            if ($(this).val() === triviaQuestions[0].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         })
 
-        $.each($("input[name='question-1']:checked"), function() {
-            if ($(this).val() == questions[1].correctAnswer) {
+        $.each($("input[name='question-1']:checked"), function () {
+            if ($(this).val() === triviaQuestions[1].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         })
 
-        $.each($("input[name='question-2']:checked"), function() {
-            if ($(this).val() == questions[2].correctAnswer) {
+        $.each($("input[name='question-2']:checked"), function () {
+            if ($(this).val() === triviaQuestions[2].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
 
-        this.result();
+        this.finalScore();
     },
 
-    result: function() {
+    finalScore: function () {
         clearInterval(timer);
         $("#inner-container h2").remove();
 
-        $("#inner-container").html("<h2>All done!</h2>");
-        $("#inner-container").append("<h3>Correct Answers: "+this.correct+"</h3>");
-        $("#inner-container").append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
-        $("#inner-container").append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
+        $("#inner-container").html("<h2>Final Score:</h2><br>");
+        $("#inner-container").append("<h2>Correct Answers: " + (this.correct) + "</h2>");
+        $("#inner-container").append("<h2>Incorrect Answers: " + (this.incorrect) + "</h2>");
+        $("#inner-container").append("<h2>Unanswered: " + (triviaQuestions.length - (this.incorrect + this.correct)) + "</h2>");
+
     }
 
 }
